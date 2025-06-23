665be3131eda55c4d06db6d582623655ce6a152f
Javi Villar <javiroll93@gmail.com>
2025-06-23 16:59:41 +0200

    internationalization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,31 @@
+export const defaultLang = "es"
+
+export const languageList = {
+    es: "ES",
+    ca: "CA",
+    en: "EN"
+}
+
+export const labels = {
+    es: {
+        'main-title': 'Hola',
+        'main-subtitle-1': 'Mi nombre es Javi,',
+        'main-subtitle-2': 'desarrollador front-end ubicado en Mallorca 🏝️',
+        'main-more-about-me': 'Si quieres saber mas sobre mi:',
+        'footer-text': 'Gracias por visitar ❤️'
+    },
+    ca: {
+        'main-title': 'Hola',
+        'main-subtitle-1': 'El meu nom es Javi,',
+        'main-subtitle-2': 'desenvolupador front-end ubicat a Mallorca 🏝️',
+        'main-more-about-me': 'Si vols saber mes sobre mi:',
+        'footer-text': 'Gràcies per visitar ❤️'
+    },
+    en: {
+        "main-title": "Hello",
+        "main-subtitle-1": "My name is Javi,",
+        "main-subtitle-2": "front-end developer based in Mallorca 🏝️",
+        "main-more-about-me": "If you want to know more about me:",
+        "footer-text": "Thanks for visiting ❤️"
+    },
+}
